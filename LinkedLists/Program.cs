ad91a143ae13eb94e43c092ec7bfa5ac66fd422e
Marcos Venicius <marcosdevelopment.business@gmail.com>
2023-02-11 17:05:20 -0300

    init

---
+++ LinkedLists/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace LinkedLists;
+
+// - [ ] write a function that prints a singly linked list on display
+// - [ ] write a function to get the last node in a singly linked list
+// - [ ] write a function that converts a singly linked list to an array
+
+public static class Program
+{
+    public static void Main()
+    {
+        var integers = new SinglyLinkedList<int>();
+        var doubles = new SinglyLinkedList<double>();
+        
+        integers.Add(1);
+        integers.Add(2);
+        integers.Add(3);
+        
+        doubles.Add(1);
+        doubles.Add(2.1);
+        doubles.Add(2.9);
+
+        Console.WriteLine(integers);
+        Console.WriteLine(integers.Sum());
+        
+        Console.WriteLine(doubles);
+        Console.WriteLine(doubles.Sum());
+        
+        var structure = new SinglyLinkedList<char>();
+        
+        structure.Add('B');
+        structure.Add('C');
+        structure.Add('D');
+        structure.AddOnTop('A');
+
+        var array = structure.ToArray();
+        
+        Console.WriteLine(structure);
+
+        Console.WriteLine($"HAS 'B': {structure.Has('B')} {structure.FindFirst('B')?.Value}");
+
+        Console.WriteLine();
+        
+        foreach (var value in array)
+            Console.WriteLine(value);
+    }
+}
---
+++ LinkedLists/SinglyLinkedList.cs
@@ -0,0 +1,145 @@
+﻿using System.Text;
+
+namespace LinkedLists;
+
+public sealed class SinglyLinkedList<T>
+{
+    public Node<T>? Head { get; private set; }
+    public Node<T>? Tail { get; private set; }
+
+    public override string ToString()
+    {
+        StringBuilder stringBuilder = new();
+
+        var current = Head;
+
+        while (current is not null)
+        {
+            stringBuilder.Append(current.Value);
+
+            if (current.Next is not null)
+                stringBuilder.Append(" --> ");
+
+            current = current.Next;
+        }
+
+        stringBuilder.AppendLine();
+
+        return stringBuilder.ToString();
+    }
+
+    public void AddOnTop(T data)
+    {
+        var node = new Node<T>(data);
+
+        if (Head is not null)
+        {
+            node.Next = Head;
+            Head = node;
+        }
+        else
+        {
+            Head = node;
+            Tail = node;
+            Head.Next = Tail;
+        }
+    }
+
+    public void Add(T data)
+    {
+        var node = new Node<T>(data);
+
+        if (Tail is not null)
+        {
+            Tail.Next = node;
+            Tail = Tail.Next;
+        }
+        else
+        {
+            Head = node;
+            Tail = node;
+            Head.Next = Tail;
+        }
+    }
+
+    public bool Has(T data) => FindFirst(data) is not null;
+
+    public Node<T>? FindFirst(T? data)
+    {
+        var current = Head;
+
+        while (current is not null)
+        {
+            if ((bool)data?.Equals(current.Value))
+                return current;
+
+            current = current.Next;
+        }
+
+        return current;
+    }
+}
+
+public static class SinglyLinkedListExt
+{
+    public static int Sum(this SinglyLinkedList<int> singlyLinkedList)
+    {
+        var sum = 0;
+
+        var current = singlyLinkedList.Head;
+
+        while (current is not null)
+        {
+            sum += current.Value;
+            current = current.Next;
+        }
+
+        return sum;
+    }
+
+    public static double Sum(this SinglyLinkedList<double> singlyLinkedList)
+    {
+        var sum = 0.0;
+
+        var current = singlyLinkedList.Head;
+
+        while (current is not null)
+        {
+            sum += current.Value;
+            current = current.Next;
+        }
+
+        return sum;
+    }
+
+    public static float Sum(this SinglyLinkedList<float> singlyLinkedList)
+    {
+        var sum = 0f;
+
+        var current = singlyLinkedList.Head;
+
+        while (current is not null)
+        {
+            sum += current.Value;
+            current = current.Next;
+        }
+
+        return sum;
+    }
+
+    public static IEnumerable<T> ToArray<T>(this SinglyLinkedList<T> singlyLinkedList)
+    {
+        List<T> list = new();
+
+        var current = singlyLinkedList.Head;
+
+        while (current is not null)
+        {
+            list.Add(current.Value);
+
+            current = current.Next;
+        }
+
+        return list;
+    }
+}
